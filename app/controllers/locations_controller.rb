3b494e627f98a24bf7e3b776e132400eeb385a09
Mitchell Bagley <bagleymj@gmail.com>
2013-02-12 15:01:36 -0500

    added new location view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,9 @@ class LocationsController < ApplicationController
   def index
     @title = "Location List"
   end
+  def new
+    @title = "Add Location"
+    @location = Location.new
+  end
+
 end
--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -20,6 +20,45 @@ describe LocationsController do
       get :index
       response.should have_selector("h1", :content => "Location List")
     end
+
+  end
+  describe "'new' location" do
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+    it "should have the correct title" do
+      get :new
+      response.should have_selector("title", :content => " | Add Location")
+    end
+    it "should have title in big letters on the page" do
+      get :new
+      response.should have_selector("h1", :content => "Add Location")
+    end
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='location[name]'][type='text']")
+    end
+    it "should have an address1 field" do
+      get :new
+      response.should have_selector("input[name='location[address1]'][type='text']")
+    end
+    it "should have an address2 field" do
+      get :new
+      response.should have_selector("input[name='location[address2]'][type='text']")
+    end
+    it "should have a city field" do
+      get :new
+      response.should have_selector("input[name='location[city]'][type='text']")
+    end
+    it "should have a state field" do
+      get :new
+      response.should have_selector("input[name='location[state]'][type='text']")
+    end
+    it "should have a zip field" do
+      get :new
+      response.should have_selector("input[name='location[zip]'][type='text']")
+    end
   end
 
 end
